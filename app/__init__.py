aff47551f76fa23fd8c40cd0cdae765ff4b4cbde
Rohan Luhar <99538858+RohanLuhar@users.noreply.github.com>
2023-10-05 04:28:01 +0530

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,74 @@
+import requests, os, uuid, json
+from flask import Flask, render_template, request
+import requests
+
+key = "a35a7b73b2374ad2896b197d61f332b8"
+endpoint = "https://api.cognitive.microsofttranslator.com"
+location = "centralindia"
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['hi','gu']
+}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key,
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        city_name = request.form['name']
+
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=3c6d6137fff8654d1f2f4cdff76b0698'
+        response = requests.get(url.format(city_name)).json()
+        
+        temp = response['main']['temp']
+        weather = response['weather'][0]['description']
+        min_temp = response['main']['temp_min']
+        max_temp = response['main']['temp_max']
+        icon = response['weather'][0]['icon']
+        print(city_name,temp,weather,min_temp,max_temp,icon)
+        
+        body = [
+            {'text': temp},
+            {'text': weather},
+            {'text': min_temp},
+            {'text': max_temp},
+            {'text': city_name},
+        ]
+
+        requestt = requests.post(constructed_url, params=params, headers=headers, json=body)
+        response = requestt.json()
+        temp_hindi = response[0]['translations'][0]['text']
+        weather_hindi = response[1]['translations'][0]['text']
+        min_temp_hindi = response[2]['translations'][0]['text']
+        max_temp_hindi = response[3]['translations'][0]['text']    
+        city_name_hindi = response[4]['translations'][0]['text']
+
+        return render_template('index.html',
+                               temp = temp,
+                               temp_hindi=temp_hindi,
+                               weather=weather.title(),
+                               weather_hindi=weather_hindi,
+                               min_temp=min_temp,
+                               min_temp_hindi=min_temp_hindi,
+                               max_temp=max_temp,
+                               max_temp_hindi=max_temp_hindi,
+                               icon=icon,
+                               city_name=city_name.upper(),
+                               city_name_hindi=city_name_hindi) 
+    
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
